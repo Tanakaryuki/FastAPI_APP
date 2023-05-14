bb4e895661fe52cc10a51115812b27868b5cc977
Tanakaryuki <tnk3257.rt@gmail.com>
2023-05-15 00:19:03 +0900

    modelの移動

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI,HTTPException
+from fastapi.responses import JSONResponse
+from databases import Database
+from pydantic import BaseModel,Field
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -16,19 +18,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = None
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items_list/{name}")
+def read_item(name:str):
+    return {"price": name}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
